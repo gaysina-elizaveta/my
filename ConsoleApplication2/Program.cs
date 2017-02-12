83d07a3e921eca26a6f2f8ef7c19d0ae55586b4a
Ксюша <@123>
2017-02-12 18:13:42 +0500

    add class point,edge,triangle

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
+    class Point
+    {
+
+    }
+    class Edge
+    {
+
+    }
+    class Triangle
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
