97c6c790497d55e7867b56e8a57ff36a25972774
Ксюша <@123>
2017-02-21 10:50:18 +0500

    готовый проект, без массива

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,20 +8,30 @@ namespace ConsoleApplication2
 {
     class Point
     {
-
+        double x;
+        double y; 
     }
     class Edge
     {
-
+        double x;
+        double y;
     }
     class Triangle
     {
+        double area;
+        double Perimeter;
+        string Type;
+        public void TypeT()
+        {
+            Console.WriteLine("Тип треугольника: " + Type);
+        }
 
     }
     class Program
     {
         static void Main(string[] args)
         {
+             
         }
     }
 }
